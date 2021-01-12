914c224a49f197574eaa1e47478a71c87a008f3d
Patryk Rusin <p.rusin@nanocoder.pl>
2021-01-12 22:44:34 +0100

    DaysPicker: tests

--- tests/unit/DaysPicker.spec.js
+++ tests/unit/DaysPicker.spec.js
@@ -1,5 +1,5 @@
-import { shallowMount } from '@vue/test-utils'
-import DaysPicker from '@/components/DaysPicker.vue'
+import {shallowMount} from '@vue/test-utils';
+import DaysPicker from '@/components/DaysPicker.vue';
 
 describe('DaysPicker.vue', () => {
   const wrapper = shallowMount(DaysPicker);
@@ -7,4 +7,18 @@ describe('DaysPicker.vue', () => {
   it('renders component', () => {
     expect(wrapper.exists()).toBe(true);
   });
-})
+
+  it('check current date is correct', () => {
+    const currentDate = new Date().toISOString().split('.')[0];
+
+    expect(wrapper.vm.$data.currentDate.toISOString().split('.')[0]).toBe(currentDate);
+  });
+
+  it('check is calendar days generated correctly', () => {
+    DaysPicker.methods.setWeekDays(wrapper.vm.$data.weekDays, 0);
+    const days = wrapper.vm.$data.calendarDays.lastMonth.length
+      + wrapper.vm.$data.calendarDays.currentMonth.length
+      + wrapper.vm.$data.calendarDays.nextMonth.length;
+    expect(days).toBe(42);
+  });
+});
